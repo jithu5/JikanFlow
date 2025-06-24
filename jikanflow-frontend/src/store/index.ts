2567504669f96357c840bb78b47cfa30490741cf
jithu5 <abijithr202@gmail.com>
2025-06-24 15:02:55 +0530

    add zustand and axios

---
+++ jikanflow-frontend/src/store/index.ts
@@ -0,0 +1,15 @@
+import { create } from 'zustand'
+
+interface StoreState {
+    count: number;
+    increaseCount: () => void;
+    decreaseCount: ()=> void;
+}
+
+const useStore = create<StoreState>((set) => ({
+    count: 0,
+    increaseCount: () => set((state) => ({ count: state.count + 1 })),
+    decreaseCount: () => set((state) => ({count: state.count - 1 }))
+}));
+
+export default useStore;
