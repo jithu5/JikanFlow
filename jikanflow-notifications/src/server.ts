3e3bb28c4d76dddc1e188a12d2da354763e91219
jithu5 <abijithr202@gmail.com>
2025-07-02 18:05:31 +0530

    added microservice for eamil and notification service with express

---
+++ jikanflow-notifications/src/server.ts
@@ -0,0 +1,23 @@
+const express = require("express");               // 👈 CommonJS require
+import { Request, Response } from "express";
+// Removed manual destructuring of Request and Response to avoid redeclaration error
+
+const app = express();
+const PORT = 4002;
+
+app.use(express.json());
+
+/**
+ * @param {Request} req
+ * @param {Response} res
+ */
+interface HelloRequest extends Request {}
+interface HelloResponse extends Response {}
+
+app.get("/", (req: HelloRequest, res: HelloResponse) => {
+  res.send("Hello from CommonJS + TypeScript!");
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
