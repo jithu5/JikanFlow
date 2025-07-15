a6c567e7d18137ac98beedd10f51c861ae3b287d
jithu5 <abijithr202@gmail.com>
2025-07-15 15:25:05 +0530

    Restructuring in microservices

---
+++ jikanflow-user/src/main/java/com/jikanflow/user/JikanflowUserApplication.java
@@ -0,0 +1,13 @@
+package com.jikanflow.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JikanflowUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JikanflowUserApplication.class, args);
+	}
+
+}
